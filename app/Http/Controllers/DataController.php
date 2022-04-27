e313e5f7a28d016169651d261d53dba5a55a0b2b
herudhece <herudwichya@gmail.com>
2022-04-28 02:58:55 +0700

    first commit

---
+++ app/Http/Controllers/DataController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Data;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+class DataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            $query = Data::query();
+            return DataTables::of($query)
+                ->addIndexColumn()
+                ->addColumn('action', 'data._action')
+                ->toJson();
+        }
+        return view('data.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Data $data)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Data $data)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Data $data)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Data $data)
+    {
+        //
+    }
+}
---
+++ resources/views/data/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card shadow mb-4">
+                <div class="card-body">
+                        <a href="{{ route('data.create') }}" class="btn btn-md btn-success mb-3">TAMBAH</a>
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th>Telpon</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@push('js')
+<script type="text/javascript">
+    $(function () {
+        
+      var table = $('#dataTable').DataTable({
+          processing: true,
+          serverSide: true,
+          ajax: "{{ route('data.index') }}",
+          columns: [
+              {data: 'id', name: 'id'},
+              {data: 'name', name: 'name'},
+              {data: 'email', name: 'email'},
+              {data: 'phone', name: 'phone'},
+              {data: 'action', name: 'action', orderable: false, searchable: false},
+          ]
+      });
+        
+    });
+  </script>
+@endpush
